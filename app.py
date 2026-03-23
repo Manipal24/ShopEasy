325521b47e722e424b0e983887d7a796baa86d85
Manipalreddy <anammanipalreddy@gmail.com>
2026-03-23 18:00:28 +0530

    add deployment files

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def place_order():
     return jsonify({"success": True, "message": "Order placed successfully!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
